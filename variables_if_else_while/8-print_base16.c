65cde73234e2bb8b4beef2000aff3a876960bbf5
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-16 07:58:51 -0700

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 48; l <= 57; l++)
+	{
+	putchar(l);
+	}
+	for (l = 97; l <= 102; l++)
+	{
+	putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
