7b554f50c20d32cc0b399f7bc29e841f0a98b90d
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-16 09:08:59 -0700

    101

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -6,33 +6,31 @@
  * Description: 'putchar'
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	int i, j, k;
 
 	for (i = 48; i <= 57; i++)
 	{
-	for (j = 48; j <= 57; j++)	
-	{
-	if (i < j)
-	{
-	for (k = 48; k <= 57; k++)
-	{
-	if (j < k)
-	{
-	putchar(i);
-	putchar(j);
-	putchar(k);
-	if (i != 55)
-	{
-	putchar(',');
-	putchar(' ');
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
 	}
-	}
-	}
-	}
-	}
-	}
-	putchar('\n');
 
+	putchar('\n');
 	return (0);
 }
