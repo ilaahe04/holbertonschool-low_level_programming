bb9b6a70c126ff63619cabe33400cd64f7a3822d
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-16 05:15:33 -0700

    ssfg

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,14 @@
 #include <time.h>
 /* more headers goes there */
 #include<stdio.h>
+
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'get number'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -10,6 +17,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+
 	if (n > 0)
 	{
 	printf("The number %d is positive.\n", n);
