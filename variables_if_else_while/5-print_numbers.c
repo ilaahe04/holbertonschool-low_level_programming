e615070a7eb276b2ec1863da77b1d1f3a83f0b8c
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-16 07:22:52 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; l++)
+	{
+	putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
