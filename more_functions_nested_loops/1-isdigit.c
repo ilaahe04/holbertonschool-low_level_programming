194bcffebcb156e29f7e3c99714cc29f21a270c1
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-19 14:09:08 -0700

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isdigit - checks
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+int _isdigit(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+
+	return (0);
+}
