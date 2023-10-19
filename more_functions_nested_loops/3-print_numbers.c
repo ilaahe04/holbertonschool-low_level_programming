744fde07a07038a0a47e402b27c1cde1a03ed6d4
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-19 14:48:05 -0700

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - prints the number
- *
- * Return: Always 0 (Success)
- *
- */
+  * print_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
 void print_numbers(void)
 {
-	int num = 48;
+	int x;
 
-	while (num <= 57)
+	for (x = 0; x <= 9; x++)
 	{
-	_putchar(num);
-	num++;
+		_putchar(x + '0');
 	}
+
 	_putchar('\n');
 }
