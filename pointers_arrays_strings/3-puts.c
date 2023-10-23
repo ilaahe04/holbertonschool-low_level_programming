2a68f814483cf2a15022e730d71dbf72566cbf41
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-23 06:43:58 -0700

    3m

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - Entry point
+ *
+ * @str: variable
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
