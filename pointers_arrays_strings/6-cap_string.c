041775e55810b306883b0f39273f967eff6cf9de
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-26 15:43:51 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * *cap_string - Entry point
+ *
+ * @upper: 'var'
+ * Return: Always 0 (Success)
+ */
+
+char *cap_string(char *upper)
+{
+	 int i;
+
+	for (i = 0; upper[i] != '\0'; i++)
+	{
+		if (i == 0 && upper[i + 1] >= 97  && upper[i] <= 122)
+			upper[i] -= 32;
+		if (upper[i + 1] >= 97 && upper[i + 1] <= 122)
+		{
+			if (upper[i] == 32 || upper[i] == 10 || upper[i] == 9)
+					upper[i + 1] -= 32;
+			else if (upper[i] == 44 || upper[i] ==  59 || upper[i] == 46)
+                                upper[i + 1] -= 32;
+			else if (upper[i] == 33 || upper[i] == 63 || upper[i] == 34)
+				upper[i + 1] -= 32;
+			else if (upper[i] == 40 || upper[i] == 41)
+				upper[i + 1] -=  32;
+			else if (upper[i] == 123 || upper[i] == 125 )
+				upper[i + 1] -= 32;
+		
+		}
+	}
+	return (upper);
+}
+int main(void)
+{
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
