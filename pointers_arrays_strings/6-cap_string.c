c5ff5551ef7362d267ee4690ec2fe20e385516e2
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-27 03:10:32 -0700

    6m

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@
 
 char *cap_string(char *upper)
 {
-	 int i;
+	int i;
 
 	for (i = 0; upper[i] != '\0'; i++)
 	{
@@ -32,14 +32,4 @@ char *cap_string(char *upper)
 		}
 	}
 	return (upper);
-}
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
 }
