e266ed76fd636b72e277069ddc0fd7713ea72ce8
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-30 05:09:07 -0700

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,10 +7,10 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] != '0')
+	if (s[0] != '\0')
 	{
-		_putschar(s[0]);
-		_puts_recursion(s + 1)
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
 	else
 	_putchar('\n');
