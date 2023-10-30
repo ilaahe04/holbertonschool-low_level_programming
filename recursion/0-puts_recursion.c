5323338b8a929de900e987a6babcb2732d3c1469
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-30 05:06:30 -0700

    put

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: the string to be printed
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] != '0')
+	{
+		_putschar(s[0]);
+		_puts_recursion(s + 1)
+	}
+	else
+	_putchar('\n');
+}
