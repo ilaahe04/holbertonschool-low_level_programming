f871d031464db7f8b41098f54c756944db9399ee
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-30 12:31:39 -0700

    4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - Calculates pow number.
+ * @x: number 
+ * @y: number
+ * Return: facorial number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+	else 
+		return (1);
+}
