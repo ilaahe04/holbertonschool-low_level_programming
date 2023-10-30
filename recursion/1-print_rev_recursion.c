5850f73dadf1afc42f9473bbe9a662ab59d767bd
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-10-30 05:22:26 -0700

    1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string, followed by a new line.
+ * @s: the string to be printed
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	 if (s[0] != '\0')
+	 {
+		_print_rev_recursion(s + 1);
+		_putchar(s[0]);
+	 }
+}
