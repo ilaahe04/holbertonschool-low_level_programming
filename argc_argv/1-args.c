0055c2c103db656271b5f4bbd514ff02ada9ee3e
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-02 05:31:18 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+
+	return (0);
+}
