82bdd079af565e066ee92575baaddf0081b8df40
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-06 04:19:01 -0800

    0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - fills memory
+ * @size: size
+ * @c: char
+ *
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{	
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+	arr = malloc( size * sizeof(char) );
+
+	if (arr == NULL)
+	{
+		return NULL;
+	}
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	
+	return (arr);
+}
