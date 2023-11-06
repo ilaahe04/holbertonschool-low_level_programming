fd6851eb6b261c3acb4dec41d9632b2d98648d14
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-06 05:28:10 -0800

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - fills memory
+ * @str: str
+ *
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char *arr;
+	int i, size = strlen(str);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = (char *)malloc(sizeof(char) * (size + 1));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		arr[i] = str[i];
+	}
+	return (arr);
+}
