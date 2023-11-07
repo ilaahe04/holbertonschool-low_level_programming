8656fc4720d983d5492f8ef9c958e6004f745e0d
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-07 04:00:41 -0800

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - fills memory
+ * @str: str
+ *
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *arr;
+	int i, size_1, size_2;
+	int size;
+
+	if (s1 == NULL)
+	{
+		size_1 = 0;
+	}
+	else
+	{
+		size_1 = strlen(s1);
+	}
+	if (s2 == NULL)
+	{
+		size_2 = 0;
+	}
+	else
+	{
+		size_2 = strlen(s2);
+	}
+	size = size_1 + size_2;
+	arr = malloc(sizeof(char) * (size_1 + size_2) + 1);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (i < size_1)
+		{
+			arr[i] = s1[i];
+		}
+		else 
+			arr[i] = s2[i - size_1];
+	}
+	arr[i] = '\0';
+	return (arr);
+}
