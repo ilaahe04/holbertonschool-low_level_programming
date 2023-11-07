f7082c2f5513cc46061956133745ab4d1e980bd6
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-07 06:17:43 -0800

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
+ * alloc_grid - allocated grid
  * @width: width of the grid
  * @height: height of the grid
  *
