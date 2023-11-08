f15d0f237111880f64f4ddc036d63108da935918
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-08 07:30:00 -0800

    3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - check the code
+ *
+ * @min: var
+ * @max: var
+ *
+ * Return: Always 0.
+ */
+int *array_range(int min, int max)
+{
+	int i, *arr;
+	
+	if (min > max)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(int) * (max - min + 1));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i <= max - min; i++)
+		arr[i] = min + i;
+	return arr;	
+}
