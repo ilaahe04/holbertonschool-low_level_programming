879fad0b52c3b6e7895f8de34796cbde77a8dfbd
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-08 07:32:39 -0800

    3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@
 int *array_range(int min, int max)
 {
 	int i, *arr;
-	
+
 	if (min > max)
 	{
 		return (NULL);
@@ -22,5 +22,5 @@ int *array_range(int min, int max)
 		return (NULL);
 	for (i = 0; i <= max - min; i++)
 		arr[i] = min + i;
-	return arr;	
+	return (arr);
 }
