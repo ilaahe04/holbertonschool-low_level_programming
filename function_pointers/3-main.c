633b9b35bcdc14631a37a97c64b72156431d7261
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-22 03:46:10 -0800

    3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+int main(int argc, char **argv)
+{
+	char *op = argv[2];
+	int a, b;
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+	if ((*op != '+' && *op != '-' && *op != '*' && *op != '/' && *op != '%')
+			|| strlen(op) > 1)
+		printf("Error\n"), exit(99);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if ((*op == '/' || *op == '%') && b == 0)
+		printf("Error\n"), exit(100);
+	printf("%d\n", get_op_func(op)(a, b));
+	return(0);
+}
