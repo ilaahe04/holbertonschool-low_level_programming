042763616eead158612277131e47dee4fd6991d5
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-07 04:24:10 -0800

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while ( h != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
@@ -21,10 +21,10 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n",h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
 		h = h->next;
 	}
-	return count;
+	return (count);
 }
