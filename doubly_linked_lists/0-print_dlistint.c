3d66c1c50f74bc20a9c3617405c5fe9188df8a77
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-09 01:48:37 -0800

    0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - check the code
+ *
+ * @h: var
+ *
+ * Return: Always 0.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
