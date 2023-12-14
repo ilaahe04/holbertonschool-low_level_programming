d0f1ab653f2b69a8c73340a0131566de064ef73f
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-14 06:19:26 -0800

    0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,15 +11,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = (hash_table_t*)malloc(sizeof(hash_table_t));
+	hash_table_t *ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
-
 	ht->size = size;
 	ht->array = malloc(8 * size);
-	if (ht->array ==  NULL)
+	if (ht->array == NULL)
 		return (NULL);
-
-	return(ht);
+	return (ht);
 }
