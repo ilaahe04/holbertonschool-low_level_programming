ff2cb40dac4ea415b5d14a848f31d9ac15765180
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-16 08:22:58 -0800

    4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,13 +1,30 @@
 #include <stdlib.h>
+#include <string.h>
 #include "hash_tables.h"
-
+/**
+ * hash_table_get - gets value from array at the given index
+ *
+ * @ht: hash table
+ *
+ * @key: key
+ *
+ * Return: string if exist, otherwise NULL
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned int index;
-	hash_table_t *new_node;
+	unsigned long int index;
 	
-	if (ht == NULL || key == NULL || *key = '\0')
+	if (ht == NULL || key == NULL)
 	{
 		return (NULL);
 	}
+
+	index = key_index((unsigned char *)key, ht->size);
+	while (!ht->array[index])
+	{
+		if (!strcmp(ht->array[index]->key, key))
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+	return (NULL);
 }
