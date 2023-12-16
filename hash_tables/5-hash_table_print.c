977313a9888d54fe7b2a31d73699ded4823f4ba7
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-16 09:28:36 -0800

    5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hash table
+ *
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p;
+	char n = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			p = ht->array[i];
+			while (p)
+			{
+				if (n > 0)
+					printf(", ");
+				printf("'%s': '%s'", p->key, p->value);
+				p = p-> next;
+				n++;
+			}
+		}
+	}
+		printf("}\n");
+}
