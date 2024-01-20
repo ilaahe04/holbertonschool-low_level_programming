862a8046a366a8abac0d6bfbe246afa9564bdaaf
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-01-20 01:08:28 -0800

    linear search algorithm

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+
 /**
  * linear_search - Searches for a value in an array of integers
  * using the Linear search algorithm.
@@ -9,18 +10,19 @@
  *
  * Return: Index of value, otherwise -1.
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i, r = -1;
+	size_t i, a = -1;
 
 	for (i = 0; array && i < size; i++)
 	{
-		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			r = i;
+			a = i;
 			break;
 		}
 	}
-	return (r);
+	return (a);
 }
